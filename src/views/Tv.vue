02bcb87dea181dd8e7d5d0137dfe5cf83756f500
donghaoyuan0030 <76894821+donghaoyuan0030@users.noreply.github.com>
2021-01-29 11:15:14 +0800

    Update Tv.vue

--- src/views/Tv.vue
+++ src/views/Tv.vue
@@ -8,14 +8,11 @@
         @click="getDetail(tv.id)"
       >
         <div class="img-box">
-          <img
-            :src="'https://images.weserv.nl/?url=' + tv.cover.url"
-            alt=""
-          >
+          <img :src="'https://images.weserv.nl/?url=' + tv.cover.url" alt="" />
         </div>
         <div class="content">
-          <h3>{{tv.title}}</h3>
-          <p>{{tv.info}}</p>
+          <h3>{{ tv.title }}</h3>
+          <p>{{ tv.info }}</p>
         </div>
       </li>
     </ul>
@@ -53,12 +50,10 @@ export default {
             }
           )
           .then((res) => {
-            // this.tvList = res.data.subject_collection_items;
-            this.tvList = this.tvList.concat(res.data.subject_collection_items);
-            // this.tvList = [
-            //   ...this.tvList,
-            //   ...res.data.subject_collection_items,
-            // ];
+            this.tvList = [
+              ...this.tvList,
+              ...res.data.subject_collection_items,
+            ];
             this.isFinish = true;
           });
       }
@@ -69,17 +64,19 @@ export default {
       let fullH = 0;
       // 设备的高度
       let deviceH = htmlDom.clientHeight;
-      console.log(deviceH);
       // 滚动出去的距离
       let scrollT = 0;
       window.onscroll = () => {
         fullH = htmlDom.offsetHeight;
+
         scrollT = htmlDom.scrollTop;
+
         // 滚动到底部
         if (deviceH + scrollT > fullH - 20) {
-        this.start += 10;
+          this.start += 10;
           if (this.start < 40) {
-            
+            // 请求下一页数据
+
             this.getData();
           }
         }
@@ -95,6 +92,7 @@ export default {
 
 <style lang="scss" scoped>
 .tv-item {
+  // margin-top: 1rem;
   display: flex;
   padding: 0.2rem 0;
   border-bottom: 1px dashed #ccc;
@@ -106,6 +104,7 @@ export default {
     }
   }
   .content {
+
     flex: 5;
     padding: 0.8rem;
     text-align: center;
